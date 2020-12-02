8998299f864a8bf2b1a6eafdf5614e93c645dcf8
Charles Vallance <charles@sharesies.com>
2020-12-02 20:07:33 +1300

    Adding train work for dotnet

---
+++ 2020/dotnet/DayOne/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace DayOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var firstFound = false;
+            var secondFound = false;
+            var firstResult = 0;
+            var secondResult = 0;
+            var lines = File.ReadLines("../inputs/dayone.txt").ToList();
+            for (var i = 0; i < lines.Count; i++)
+            {
+                var firstValue = int.Parse(lines[i]);
+                for (var j = i + 1; j < lines.Count; j++)
+                {
+                    var secondValue = int.Parse(lines[j]);
+                    if (firstValue + secondValue == 2020)
+                    {
+                        firstResult = firstValue * secondValue;
+                        firstFound = true;
+                        break;
+                    }
+                    
+                    for (var k = j + 1; k < lines.Count; k++)
+                    {
+                        var thirdValue = int.Parse(lines[k]);
+                        if (firstValue + secondValue + thirdValue == 2020)
+                        {
+                            secondResult = firstValue * secondValue * thirdValue;
+                            secondFound = true;
+                            break;
+                        }
+                    }
+                }
+                
+                if (firstFound && secondFound)
+                {
+                    break;
+                }
+            }
+            
+            Console.WriteLine($"Day 1 - Part 1: {firstResult}");
+            Console.WriteLine($"Day 1 - Part 2: {secondResult}");
+        }
+    }
+}
