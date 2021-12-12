f08bd76a557d67c7b7ab21da67b5f4da78ed8799
Charles Vallance <vallance.charles@gmail.com>
2021-12-12 21:08:08 +1300

    Day12

---
+++ 2021/dotnet/Day12/Node.cs
@@ -0,0 +1,75 @@
+namespace Day12;
+
+public struct Node
+{
+    public string Name { get; init; }
+    public bool IsLarge => Name.ToUpper() == Name;
+
+    public HashSet<Node> Children = new();
+
+    public override int GetHashCode()
+    {
+        return Name.GetHashCode();
+    }
+}
+
+public struct Path
+{
+    public bool DoubleUsed = false;
+    public List<Node> Nodes { get; init; } = new();
+    public HashSet<Node> SmallNodes { get; init; } = new();
+
+    public Node TailNode => Nodes[^1];
+
+    public Path() { }
+    public Path(Node startingNode)
+    {
+        Nodes.Add(startingNode);
+    }
+    
+    public bool IsComplete()
+    {
+        if (Nodes.Count == 0) return false;
+
+        return TailNode.Name == "end";
+    }
+
+    public bool AddNode(Node node)
+    {
+        if (!node.IsLarge)
+        {
+            if (node.Name == "start") return false;
+            
+            if (SmallNodes.Contains(node))
+            {
+                if (DoubleUsed) return false;
+
+                DoubleUsed = true;
+            }
+            SmallNodes.Add(node);
+        }
+
+        Nodes.Add(node);
+        return true;
+    }
+    
+    public Path Clone()
+    {
+        return new Path {Nodes = Nodes.ToList(), SmallNodes = new HashSet<Node>(SmallNodes), DoubleUsed = DoubleUsed};
+    }
+
+    public override int GetHashCode()
+    {
+        var hashCode = 0;
+        foreach (var node in Nodes)
+        {
+            hashCode = HashCode.Combine(hashCode, node.GetHashCode());
+        }
+        return hashCode;
+    }
+
+    public override string ToString()
+    {
+        return string.Join(" -> ", Nodes.Select(x => x.Name));
+    }
+}
--- 2021/dotnet/Day12/Program.cs
+++ 2021/dotnet/Day12/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Day12;
+
+// var lines = File.ReadLines("../../inputs/day12-test.txt").ToList();
+var lines = File.ReadLines("../../inputs/day12.txt").ToList();
+
+var nodes = new Dictionary<string, Node>();
+foreach (var line in lines)
+{
+    var parts = line.Split("-");
+    var parent = new Node {Name = parts[0]};
+    if (nodes.ContainsKey(parent.Name))
+    {
+        parent = nodes[parent.Name];
+    }
+    else
+    {
+        nodes.Add(parent.Name, parent);
+    }
+    
+    var child = new Node {Name = parts[1]};
+    if (nodes.ContainsKey(child.Name))
+    {
+        child = nodes[child.Name];
+    }
+    else
+    {
+        nodes.Add(child.Name, child);
+    }
+    
+    parent.Children.Add(child);
+    child.Children.Add(parent);
+}
+
+var completePaths = new List<Day12.Path>();
+Action<Day12.Path> findPaths = null!;
+findPaths = currentPath =>
+{
+    var tailNode = currentPath.TailNode;
+    foreach (var childNode in tailNode.Children)
+    {
+        var newPath = currentPath.Clone();
+        
+        if (!newPath.AddNode(childNode)) continue;
+        
+        if (newPath.IsComplete())
+        {
+            completePaths.Add(newPath);
+            continue;
+        }
+
+        findPaths(newPath);
+    }
+};
+
+if (!nodes.ContainsKey("start")) throw new Exception("????");
+
+var startingPath = new Day12.Path(nodes["start"]);
+findPaths(startingPath);
+
+Console.WriteLine(completePaths.Count);
