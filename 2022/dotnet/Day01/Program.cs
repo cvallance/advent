4bc4f21f2a6399154dfb937e06772fb2e9973fa7
Charles Vallance <charles@sharesies.com>
2022-12-01 21:12:20 +1300

    Day 1 2022

---
+++ 2022/dotnet/Day01/Program.cs
@@ -0,0 +1,20 @@
+﻿var lines = File.ReadLines("../../inputs/day1.txt").ToList();
+var elves = new List<int>();
+
+var currentElve = 0;
+foreach (var line in lines)
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        elves.Add(currentElve);
+        currentElve = 0;
+        continue;
+    }
+
+    currentElve += int.Parse(line);
+}
+
+if (currentElve > 0) elves.Add(currentElve);
+
+Console.WriteLine($"Part 1: {elves.Max()}");
+Console.WriteLine($"Part 1: {elves.OrderByDescending(x => x).Take(3).Sum()}");
