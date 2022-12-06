5b17753e79b2e037519bb66a4725f00f1d613313
Charles Vallance <charles@sharesies.com>
2022-12-06 20:34:01 +1300

    Day6

--- 2022/dotnet/Day06/Solver.cs
+++ 2022/dotnet/Day06/Solver.cs
@@ -4,10 +4,21 @@ public static class Solver
 {
     public static (int, int) Solve(string data)
     {
-        var lines = data.Split("\n").ToList();
-
         var part1 = 0;
         var part2 = 0;
+        for (var i = 0; i < data.Length - 4; i++)
+        {
+            var set = new HashSet<char>(data.Skip(i).Take(4));
+            var setTwo = new HashSet<char>(data.Skip(i).Take(14));
+            if (part1 == 0 && set.Count == 4)
+            {
+                part1 = i + 4;
+            }
+            if (setTwo.Count == 14)
+            {
+                part2 = i + 14;
+            }
+        }
         
         //TODO
         
