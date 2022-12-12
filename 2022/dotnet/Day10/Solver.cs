9bde6b8d19e75061554da9c1f75316c667630f9e
Charles Vallance <charles@sharesies.com>
2022-12-13 07:01:19 +1300

    Day10 WIP

--- 2022/dotnet/Day10/Solver.cs
+++ 2022/dotnet/Day10/Solver.cs
@@ -6,10 +6,53 @@ public static class Solver
     {
         var lines = data.Split("\n").ToList();
 
-        var part1 = 0;
         var part2 = 0;
+
+        var signals = new List<int> {1};
         
-        //TODO
+        var cycle = 1;
+        foreach (var line in lines)
+        {
+            var instruction = line[..4];
+            if (instruction == "noop")
+            {
+                cycle += 1;
+                signals.Add(signals[cycle - 2]);
+            }
+            else
+            {
+                var value = int.Parse(line[5..]);
+                cycle++;
+                signals.Add(signals[cycle - 2]);
+                cycle++;
+                signals.Add(signals[cycle - 2] + value);
+            }
+        }
+
+        var part1 = 0;
+        for (var i = 0; i < signals.Count; i++)
+        {
+            if (i < 40)
+            {
+                if (i + 1 == 20)
+                {
+                    part1 += signals[i] * (i + 1);
+                }
+            }
+            else if ((i - 19) % 40 == 0)
+            {
+                part1 += signals[i] * (i + 1);
+            }
+        }
+        
+        for (var i = 0; i < signals.Count; i++)
+        {
+            if (i % 40 == 0)
+            {
+                Console.WriteLine("");
+            }
+            Console.Write("#");
+        }
         
         return (part1, part2);
     }
