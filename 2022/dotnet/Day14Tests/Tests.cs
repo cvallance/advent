950639326ec4848ec8e0a6c419b06af5fa667d90
Charles Vallance <charles@sharesies.com>
2022-12-14 22:21:42 +1300

    Day14 setup

--- 2022/dotnet/Day14Tests/Tests.cs
+++ 2022/dotnet/Day14Tests/Tests.cs
@@ -4,13 +4,14 @@ namespace Day14Tests;
 
 public class Tests
 {
-    private const string TestInput = @"";
+    private const string TestInput = @"498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9";
     
     [Fact]
     public void TestPart1()
     {
         // Arrange
-        var expected = 0;
+        var expected = 24;
 
         // Act
         var (part1, _) = Solver.Solve(TestInput);
